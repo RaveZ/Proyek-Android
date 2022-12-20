553dc06eaa4849cf00fbc97d112c67df5408e3f9
bubleygum <63784697+bubleygum@users.noreply.github.com>
2022-12-20 21:10:20 +0700

    login page
    login page di main activity

--- app/src/main/java/com/example/proyek_android/MainActivity.kt
+++ app/src/main/java/com/example/proyek_android/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.proyek_android
-
+//login page
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnLogin = findViewById<Button>(R.id.btnLogin)
+        var etUName = findViewById<EditText>(R.id.etUName)
+        var etPassword = findViewById<EditText>(R.id.etPassword)
+        var warning = findViewById<TextView>(R.id.warning)
+        var unameFound = false
+        var passFound = false
+        btnLogin.setOnClickListener {
+            if(unameFound && passFound){
+                warning.visibility = View.GONE
+            }else{
+                warning.visibility = View.VISIBLE
+            }
+        }
     }
 }
